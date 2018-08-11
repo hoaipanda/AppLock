9e362b023cdec432603f19340a79bc877c394567
TranThuHoai <hoaittt@tapbitech.com>
2018-08-11 15:07:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pc/applock/CustomUsageStats.java
@@ -0,0 +1,9 @@
+package com.example.pc.applock;
+
+import android.app.usage.UsageStats;
+import android.graphics.drawable.Drawable;
+
+public class CustomUsageStats {
+    public UsageStats usageStats;
+    public Drawable appIcon;
+}
